0b5437623c8c2aa753df712544b1c0f9d0e12da1
Dylan Beckwith <dylan.beckwith@dialexa.com>
2022-12-15 15:03:37 -0600

    PWB-18: Clean up resume resolver

--- src/resolvers/resume.resolver.ts
+++ src/resolvers/resume.resolver.ts
@@ -1,7 +1,6 @@
-import { Resolver, Query, Mutation, Args, Context, ResolveField, Parent } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
-import { SkillGroupInput } from 'inputs/skillGroup.input';
 import { SkillGroup } from 'entities/resume/skillGroup.entity';
 import { Experience } from 'entities/resume/experience.entity';
 import { Responsibility } from 'entities/resume/responsibility.entity';
@@ -9,14 +8,9 @@ import { Education } from 'entities/resume/education.entity';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from 'guards/auth.guard';
 import { AuthService } from 'services/auth.service';
-import { ExperienceInput } from 'inputs/experience.input';
-import { EducationInput } from 'inputs/education.input';
-import { ResponsibilityInput } from 'inputs/responsibility.input';
-import { UserInputError } from 'apollo-server-express';
 import { Resume } from 'models/resume.model';
 import { User } from 'entities/user/user.entity';
 import { ResumeInput } from 'inputs/resume.input';
-import { group } from 'console';
 
 @Resolver(() => Resume)
 export class ResumeResolver {
@@ -31,8 +25,9 @@ export class ResumeResolver {
 
   @Query(() => Resume, { name: 'resume' })
   async getResume() {
-    const resume = new Resume();
+    //Always get admin (Dylan Beckwith) resume
     const admin = await this.userRepository.findOneBy({ role: 'ADMIN' });
+    const resume = new Resume();
     resume.skillGroupList = await this.skillGroupRepository.findBy({ userId: admin.id });
     resume.experienceList = await this.experienceRepository.findBy({ userId: admin.id });
     for (const experience of resume.experienceList) {
@@ -52,12 +47,14 @@ export class ResumeResolver {
     const skillGroupIds = skillGroups.map((group) => group.id);
     const resumeSkillGroupIds = resume.skillGroups.map((group) => group.id);
 
+    ////Delete Skills
     skillGroupIds.forEach(async (id) => {
       if (!resumeSkillGroupIds.includes(id)) {
         await this.skillGroupRepository.delete({ id });
       }
     });
 
+    ////Save Skills
     if (resume.skillGroups) {
       for (const skillGroup of resume.skillGroups) {
         await this.skillGroupRepository.save({ ...skillGroup, userId });
@@ -111,12 +108,14 @@ export class ResumeResolver {
     const educationIds = education.map((edu) => edu.id);
     const resumeEducationIds = resume.education.map((edu) => edu.id);
 
+    ////Delete Education
     educationIds.forEach(async (id) => {
       if (!resumeEducationIds.includes(id)) {
         await this.educationRepository.delete({ id });
       }
     });
 
+    ////Save Education
     if (resume.education) {
       for (const education of resume.education) {
         await this.educationRepository.save({ ...education, userId });
@@ -124,69 +123,5 @@ export class ResumeResolver {
     }
 
     return true;
-  }
-
-  @Mutation(() => Boolean)
-  @UseGuards(AuthGuard)
-  async upsertSkillGroup(@Args('data') skillGroupInput: SkillGroupInput, @Context('req') req) {
-    const userId = this.authService.getUserId(req);
-    await this.skillGroupRepository.upsert({ ...skillGroupInput, userId }, ['id']);
-    return true;
-  }
-
-  @Query(() => [SkillGroup], { name: 'skillGroups' })
-  @UseGuards(AuthGuard)
-  async getSkillGroups(@Context('req') req) {
-    const userId = this.authService.getUserId(req);
-    return await this.skillGroupRepository.findBy({ userId });
-  }
-
-  @Mutation(() => Boolean)
-  @UseGuards(AuthGuard)
-  async upsertExperience(@Args('data') experienceInput: ExperienceInput, @Context('req') req) {
-    const userId = this.authService.getUserId(req);
-    await this.experienceRepository.upsert({ ...experienceInput, userId }, ['id']);
-    return true;
-  }
-
-  @Query(() => [Experience], { name: 'experience' })
-  @UseGuards(AuthGuard)
-  async getExperience(@Context('req') req) {
-    const userId = this.authService.getUserId(req);
-    return await this.experienceRepository.findBy({ userId });
-  }
-
-  @Mutation(() => Boolean)
-  @UseGuards(AuthGuard)
-  async upsertResponsibility(@Args('data') responsibilityInput: ResponsibilityInput, @Context('req') req) {
-    const userId = this.authService.getUserId(req);
-    const experience = await this.experienceRepository.findOneBy({ userId, id: responsibilityInput.experienceId });
-    if (!experience) return false;
-    await this.responsibilityRepository.upsert({ ...responsibilityInput }, ['id']);
-    return true;
-  }
-
-  @Query(() => [Responsibility], { name: 'responsibilities' })
-  @UseGuards(AuthGuard)
-  async getResponsibilities(@Args('experienceId') experienceId: string, @Context('req') req) {
-    const userId = this.authService.getUserId(req);
-    const experience = await this.experienceRepository.findOneBy({ userId, id: experienceId });
-    if (!experience) throw new UserInputError('experienceId does not match for any experience owned by this user');
-    return await this.responsibilityRepository.findBy({ experienceId });
-  }
-
-  @Mutation(() => Boolean)
-  @UseGuards(AuthGuard)
-  async upsertEducation(@Args('data') educationInput: EducationInput, @Context('req') req) {
-    const userId = this.authService.getUserId(req);
-    await this.educationRepository.upsert({ ...educationInput, userId }, ['id']);
-    return true;
-  }
-
-  @Query(() => [Education], { name: 'education' })
-  @UseGuards(AuthGuard)
-  async getEducation(@Context('req') req) {
-    const userId = this.authService.getUserId(req);
-    return await this.educationRepository.findBy({ userId });
   }
 }
