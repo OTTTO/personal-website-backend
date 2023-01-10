d9cef719d16c0d936ccac83041d8ad3fbd8fd592
Dylan Beckwith <dylan.beckwith@dialexa.com>
2023-01-10 15:01:49 -0600

    PWB-49: Add authenticate endpoint

--- src/resolvers/auth.resolver.ts
+++ src/resolvers/auth.resolver.ts
@@ -1,4 +1,6 @@
+import { UseGuards } from '@nestjs/common';
 import { Resolver, Query, Args } from '@nestjs/graphql';
+import { AuthGuard } from 'guards/auth.guard';
 import { AuthService } from 'services/auth.service';
 
 @Resolver()
@@ -8,5 +10,11 @@ export class AuthResolver {
   @Query(() => String, { name: 'signInAdmin' })
   async signInAdmin(@Args('email') email: string, @Args('password') password: string) {
     return await this.authService.signInAdmin(email, password);
+  }
+
+  @Query(() => Boolean, { name: 'authenticate' })
+  @UseGuards(AuthGuard)
+  async authenticate() {
+    return true;
   }
 }
